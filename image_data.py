3e5f0e8bd3fca0096fa1545c34ea801a2c721d59
Pete R Jemian <prjemian@gmail.com>
2022-09-06 19:38:07 -0500

    WIP #14 images not right, other info OK

--- image_data.py
+++ image_data.py
@@ -3,16 +3,16 @@ Read a variety of image file formats as input for tiled.
 """
 
 from PIL import Image
-from PIL import UnidentifiedImageError
 from tiled.adapters.array import ArrayAdapter
+from tiled.adapters.mapping import MapAdapter
 import numpy
 import pathlib
+import yaml
 
 ROOT = pathlib.Path(__file__).parent
 
 EXTENSIONS = []
 # https://mimetype.io/all-types#image
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 MIMETYPES = """
     image/avif
     image/bmp
@@ -25,57 +25,93 @@ MIMETYPES = """
 """.split()
 # TODO:     image/svg+xml  not handled by PIL
 
+EMPTY_ARRAY = numpy.array([0,0])
+
+
+def interpret_exif(image):
+    from PIL.ExifTags import TAGS
+    from PIL.TiffImagePlugin import IFDRational
+
+    exif = image.getexif()
+    md = {}
+    for tag_id in exif:
+        # get the tag name, instead of human unreadable tag id
+        tag = TAGS.get(tag_id, tag_id)
+        data = exif.get(tag_id)
+        # decode bytes 
+        if isinstance(data, bytes):
+            data = data.decode()
+        if isinstance(data, IFDRational):
+            attrs = "numerator denominator imag".split()
+            data = {k: getattr(data, k) for k in attrs}
+        md[tag] = data
+    return md
+
+
+def image_metadata(image):
+    attrs = """
+        bits
+        filename
+        format
+        format_description
+        is_animated
+        layer
+        layers
+        mode
+        n_frames
+        size
+        text
+    """.split()
+    md = {k: getattr(image, k) for k in attrs if hasattr(image, k)}
+
+    # TODO: "image.info" will need special handling
+    if "info" in md:
+        for k in "exif icc_profile xmp".split():
+            if k in md["info"]:
+                md["info"].pop(k)
+
+    exif = interpret_exif(image)
+    if len(exif) > 0:
+        md["exif"] = exif
+
+    md["extrema"] = image.getextrema()
+
+    return md
+
 
 def read_image(filename):
-    if filename.name.endswith(".svg"):
-        pass
     try:
         image = Image.open(filename)
-        attrs = """
-            bits
-            filename
-            format
-            format_description
-            info
-            is_animated
-            layer
-            layers
-            mode
-            n_frames
-            size
-            text
-        """.split()
-        md = {k: getattr(image, k) for k in attrs if hasattr(image, k)}
+        md = image_metadata(image)
 
         # special cases
         if image.format == "GIF":
             pass
-        elif image.format == "JPEG":
-            exif = image.getexif()
-            if len(exif) > 0:
-                md["exif"] = {k: v for k, v in exif.items()}
         elif image.format == "SVG":
             pass
-        elif image.format == "WEBP":
-            if "info" in md:
-                for k in "exif icc_profile xmp".split():
-                    if k in md["info"]:
-                        md["info"].pop(k)
-                exif = image.getexif()
-                if len(exif) > 0:
-                    md["exif"] = {k: v for k, v in exif.items()}
 
         im = image.getdata()
-        md["extrema"] = image.getextrema()
         if im.bands == 1:
             dimensions = im.size
         else:
             dimensions = (im.bands, *im.size)
         pixels = numpy.array(list(im)).reshape(dimensions)
-        print(md)
+        print(yaml.dump(md))
         return ArrayAdapter.from_array(pixels, metadata=md)
-    except UnidentifiedImageError:
-        return  ArrayAdapter.from_array(numpy.array([]), metadata={})
+
+    except Exception as exc:
+        arrays = dict(
+            ignore=ArrayAdapter.from_array(
+                numpy.array([0,0]), metadata=dict(ignore="placeholder, ignore")
+            )
+        )
+        return MapAdapter(
+            arrays, metadata=dict(
+                filename=str(filename),
+                exception=exc,
+                purpose="some problem reading this file as an image"
+            )
+        )
 
 
 def main():
